79be89b7b9c66935ec8f49fcd8ca21d116f2ce65
Harshvardhanp4 <harshupatil.224@gmail.com>
2024-12-24 09:02:53 +0530

    new additions new methods

--- solve_problems_array/largest_element.cpp
+++ solve_problems_array/largest_element.cpp
@@ -1,29 +1,69 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-int largestElement(vector<int> &arr, int n) {
-    int largest = arr[0];
-    for (int i = 0; i < n; i++) {
-        if (arr[i] > largest) {
-            largest = arr[i];
+// int largestElement(vector<int> &arr, int n) {
+//     int largest = arr[0];
+//     for (int i = 0; i < n; i++) {
+//         if (arr[i] > largest) {
+//             largest = arr[i];
+//         }
+//     }
+//     return largest;
+// }
+
+// int main() {
+//     int n;
+//     cout << "Enter array size: ";
+//     cin >> n;
+
+//     vector<int> arr(n);
+//     cout << "Enter array elements:\n";
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     int result = largestElement(arr, n);
+//     cout << "The largest element is: " << result << endl;
+
+//     return 0;
+// }
+
+// brute force method
+
+#include<bits/stdc++.h>
+using namespace std;
+//3,2,1,5,2
+ void SortArray(int arr[],int n){
+   for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+            // Swap elements
+            int temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
         }
     }
-    return largest;
 }
 
-int main() {
-    int n;
-    cout << "Enter array size: ";
-    cin >> n;
+ }
 
-    vector<int> arr(n);
-    cout << "Enter array elements:\n";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+
+int main(){
+    int n;
+    cout<<"Enter the array size: ";
+    cin>>n;
+
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
     }
 
-    int result = largestElement(arr, n);
-    cout << "The largest element is: " << result << endl;
-
-    return 0;
+    SortArray(arr,n);
+    cout<<"Sorted array: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"Largest element is: "<<arr[n-1]<<endl;
 }
--- solve_problems_array/second_largest_without_sorting.cpp
+++ solve_problems_array/second_largest_without_sorting.cpp
@@ -45,8 +45,63 @@ int main(){
             cout<<"No second largest exists! ";
         }
         else{
-            cout<<"The second largest element is"<<result<<endl;
+            cout<<"The second largest element is: "<<result<<endl;
 
         }
         return 0;
+}
+
+//abovee method is a brute force method
+
+//below we will do simple ones and optimal ones
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int secondLargest(vector<int>& a, int n) {
+    int largest = a[0];
+    int slargest = -1;
+    for (int i = 1; i < n; i++) {
+        if (a[i] > largest) {
+            slargest = largest;
+            largest = a[i];
+        } else if (a[i] < largest && a[i] > slargest) {
+            slargest = a[i];
+        }
+    }
+    return slargest;
+}
+
+int secondSmallest(vector<int>& a, int n) {
+    int smallest = a[0];
+    int ssmallest = INT_MAX;
+    for (int i = 1; i < n; i++) {
+        if (a[i] < smallest) {
+            ssmallest = smallest;
+            smallest = a[i];
+        } else if (a[i] != smallest && a[i] < ssmallest) {
+            ssmallest = a[i];
+        }
+    }
+    return ssmallest;
+}
+
+vector<int> getSecondOrderElements(int n, vector<int> a) {
+    int sLargest = secondLargest(a, n);
+    int sSmallest = secondSmallest(a, n);
+    return {sLargest, sSmallest};
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    vector<int> result = getSecondOrderElements(n, a);
+    cout << result[0] << " " << result[1] << endl;
+
+    return 0;
 }
