3e0a2da196c74969430bce7852c20e1b8866a6e8
Harshvardhanp4 <harshupatil.224@gmail.com>
2024-12-25 19:15:37 +0530

    with vectors

--- solve_problems_array/check_array_is_sorted.cpp
+++ solve_problems_array/check_array_is_sorted.cpp
@@ -1,40 +1,76 @@
-//1,2,2,3,3,4//
-//1,2,1,3,4//
+// //1,2,2,3,3,4//
+// //1,2,1,3,4//
 
 
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// bool isSorted(int arr[],int n){
+        
+//         for(int i=0;i<n-1;i++){
+//             if(arr[i]>arr[i+1]){
+//                 return false;
+//             }
+//         }        
+//         return true;
+//  }
+
+
+// int main(){
+//     int n;
+//     cout<<"Enter the array size ";
+//     cin>>n;
+
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+
+//     }
+
+//     if(isSorted(arr,n)){
+//         cout<<"The array is  sorted. "<<endl;
+//     }
+//     else{
+//         cout<<"The array is not sorted!"<<endl;
+
+//     }
+
+//      return 0;
+
+// }
+
+//with vectors.
+#include <bits/stdc++.h>
 using namespace std;
 
-bool isSorted(int arr[],int n){
-        
-        for(int i=1;i<n;i++){
-            if(arr[i]>=arr[i-1]){
-                return false;
-            }
-        }        
-        return true;
- }
-
-
-int main(){
-    int n;
-    cout<<"Enter the array size ";
-    cin>>n;
-
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-
+int isSorted(int n, vector<int> a) {
+    for(int i = 1; i < n; i++) {
+        if(a[i] <= a[i - 1]) {
+            return false;
+        }
     }
-
-    if(isSorted(arr,n)){
-        cout<<"The array is  sorted. "<<endl;
-    }
-    else{
-        cout<<"The array is not  sorted!"<<endl;
-
-    }
-
-     return 0;
-
+    return true;
 }
+
+int main() {
+    int n;
+    cout<<"Enter the size ";
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    if(isSorted(n, a)) {
+        cout << "The array is sorted." << endl;
+    } else {
+        cout << "The array is not sorted." << endl;
+    }
+
+    return 0;
+}
+
+
+
+
+
