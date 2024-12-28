83bbba02019a0f1426ab8939340129504371e39c
Harshvardhanp4 <harshupatil.224@gmail.com>
2024-12-29 00:14:52 +0530

    vectors

--- solve_problems_array/Left_Rotate_an_array_by_one_place.cpp
+++ solve_problems_array/Left_Rotate_an_array_by_one_place.cpp
@@ -29,4 +29,35 @@ int main() {
 }
 
 /// vector 
-#include <bits/stdc++.h>
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void leftRotateByOne(vector<int>& vec) {
+    if (vec.empty()) return;
+
+    int first = vec[0];
+    for (int i = 0; i < vec.size() - 1; ++i) {
+        vec[i] = vec[i + 1];
+    }
+    vec[vec.size() - 1] = first;
+}
+
+int main() {
+    vector<int> vec = {1, 2, 3, 4, 5};
+
+    cout << "Original vector: ";
+    for (int num : vec) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    leftRotateByOne(vec);
+
+    cout << "Vector after left rotation by one: ";
+    for (int num : vec) {
+        cout << num << " ";
+    }
+    return 0;
+}
